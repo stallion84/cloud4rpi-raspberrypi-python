408bac73b5d546145017c9e1f1c78547a55936ff
Sky3d <sky3ddd@gmail.com>
2017-12-20 15:22:42 +0300

    cpu temp as float, extend rpi

--- rpi.py
+++ rpi.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from os import uname
+from socket import gethostname
 import subprocess
+import locale
 import re
 
 
@@ -10,7 +13,8 @@ def parse_output(pattern, default, args):
         if isinstance(out_str, bytes):
             out_str = out_str.decode()
     except Exception:
-        out_str = 'error'
+        out_str = ''
+
     match = re.search(pattern, out_str)
     if match:
         result = match.group(1)
@@ -20,8 +24,17 @@ def parse_output(pattern, default, args):
 
 
 def cpu_temp():
-    return parse_output(r'temp=(\S*)\'C', '0', ['vcgencmd', 'measure_temp'])
+    t_str = parse_output(r'temp=(\S*)\'C', '0', ['vcgencmd', 'measure_temp'])
+    return locale.atof(t_str)
 
 
 def ip_address():
     return parse_output(r'(\S*)', '?', ['hostname', '-I'])
+
+
+def host_name():
+    return gethostname()
+
+
+def os_name():
+    return " ".join(uname())
